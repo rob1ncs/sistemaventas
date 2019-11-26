fa302ea482a38eb14264fb4b2684d36ab089fbb1
Robinson <rfuentes@ewarecorp.com>
2019-11-26 15:12:14 -0400

    Se arregla tabla

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="margin-top:80px;">
     <a href="{{ url('productos/create') }}">Agregar producto</a>
     <hr>
     <div class="table-responsive">
